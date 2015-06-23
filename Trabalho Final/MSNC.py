c5cec811a6c74b9a70f004145284b92451eae91a
Kizzy Fernanda Terra Ferreira dos Reis <kizzyterra@gmail.com>
2015-06-23 08:20:43 -0300

    up

--- Trabalho Final/MSNC.py
+++ Trabalho Final/MSNC.py
@@ -34,32 +34,8 @@ def quadratic(L):
 	max_subsequence.reverse()                             
 	return max_subsequence
 
-def linear(L):
-
-	sizeL = len(L)
-	if sizeL < 1:
-		return L
-	
-	subsequence = []
-	for i in range(n):          
-		subsequence.append(-1) 
-
-	incl = L[0]
-	excl = 0
-	for i in range(1, sizeL): 
-		if incl< excl:
-			exclNew = excl
-		else:
-			exclNew = incl
-			subsequence[i] = i-1
-   		  
-    	incl = excl + L[i]  
-    	excl = exclNew 
-
-    if(excl) 
-   
 	return max(incl, excl)   
 
 
 if __name__ == '__main__':
-	print linear([1,2,3,4,5])
+	print quadratic([1,2,3,4,5])
